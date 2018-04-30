5e1ae871946f0e1a3c51474af623b17511b9ca9c
thaorell <khanhchuong7396@gmail.com>
2018-04-30 14:07:30 -0400

    updated docs

--- models/trip.js
+++ models/trip.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tripSchema = new Schema({
-  userId: { type: String, required: true },
-  name: { type: String, required: true },
-  restaurants: { type: Array, required: false },
-  isComplete: { type: Boolean, required: false },
+  userId: { type: String, required: true }, //store the user who created this trip
+  name: { type: String, required: true }, //store the name of the trip
+  restaurants: { type: Array, required: false }, //store the restaurants visited during this trip
+  isComplete: { type: Boolean, required: false } //whether this trip is current(this is optional)
 });
 
 module.exports = mongoose.model('Trip', tripSchema);
